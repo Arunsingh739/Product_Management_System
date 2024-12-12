9a4930c7ee9c5f3baa6b59b601194590aeabbb98
Arun Singh <arun739singh@gmail.com>
2024-12-13 02:01:13 +0530

    Add files via upload

---
+++ product management system/Connection.java
@@ -0,0 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import com.mysql.cj.jdbc.Driver;
+
+public class LaptopConnection {
+
+	public static Connection getLaptopConnection() {
+		try {
+			//step-1 load/register driver
+			Driver driver = new Driver();
+			DriverManager.registerDriver(driver);
+			//step-2 create connection
+			String url = "jdbc:mysql://localhost:3306/jdbc-a5";
+			String user = "root";
+			String pass = "9638";
+			
+			return DriverManager.getConnection(url, user, pass);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
